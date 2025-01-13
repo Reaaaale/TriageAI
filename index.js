193a932ed067f53e2e8410ce4e379e14d8a2a5ea
Reaaaale <136179655+Reaaaale@users.noreply.github.com>
2025-01-13 21:27:54 +0100

    Modificato il backend per richiesta POST da file HTML e aggiunto lo stile al file

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const { exec } = require('child_process');
+const path = require('path'); // Importa il modulo 'path'
+
 
 const app = express();
 
 app.use(bodyParser.json());  //Permette di leggere i dati in formato JSON
-app.use(cors());  //Permette le richieste da domini diversi
+app.use(cors());
 
+// Servire il file HTML
 app.get('/', (req, res) => {
-  res.send('Server is running');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.post("/predict", (req, res) => {
@@ -23,14 +27,23 @@ app.post("/predict", (req, res) => {
     pressione_diastolica
 } = req.body;
 
-console.log("Dati ricevuti",req.body);
+const command = 'python3 inferenza.py  ${frequenza_cardiaca} ${frequenza_respiratoria} ${temperatura} ${saturazione_ossigeno} ${dolore} ${stato_coscienza} ${pressione_sistolica} ${pressione_diastolica}';
+exec (command, (error,stout ,stderr) => {
+  if (error) {
+    console.error(`exec error: ${error.message}`);
+  
+    return res.status(500).send("Errore nell'inferenza del modello");
+  }
+  if (stderr) {
+    console.error(`Stderr: ${stderr}`);
+    return res.status(500).send("Errore nello script Python.");
+}
 
-const prediction = "Mettere l'inferenza in pyhton";
-
+const prediction = stdout.trim();
 res.json({ prediction });
 
 });
-
+});
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
