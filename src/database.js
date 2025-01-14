42e0674ff809af0ceee5d47d3608640e77553abf
Reaaaale <136179655+Reaaaale@users.noreply.github.com>
2025-01-15 00:13:02 +0100

    Aggiornamento file .env

--- src/database.js
+++ src/database.js
@@ -1,11 +1,13 @@
+require('dotenv').config(); // Importa dotenv
+
 const mysql = require('mysql2');
 
-// Configurazione del database
 const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'Juventus.34',
-    database: 'triage_ml',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306, // Default al 3306 se DB_PORT non è definito
 }).promise();
 
 module.exports = db;
